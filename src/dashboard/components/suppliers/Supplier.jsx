3e056f8edfc76664bef50de7adb889bd54551af8
CarlosGarciaG <carlinigg@hotmail.com>
2024-02-15 19:37:54 -0600

    Link en carrusel
    Co-authored-by: Bruno Jimenez <Brunox117@users.noreply.github.com>

--- src/dashboard/components/suppliers/Supplier.jsx
+++ src/dashboard/components/suppliers/Supplier.jsx
@@ -7,12 +7,23 @@ import {
 } from "@mui/material";
 
 export const Supplier = ({ supplier }) => {
+  // Función para abrir el enlace en una nueva ventana
+  const openLinkInNewTab = (url) => {
+    window.open(url, "_blank");
+  };
+
   return (
     <Grid container justifyContent="center" alignItems="center">
       <Card style={{ border: "none", boxShadow: "none" }}>
-        <a href={supplier.siteLink} target="_blank">
-          <CardMedia sx={{ height: 150, width: 300}} image={supplier.imageUrl} title={supplier.name} />
-        </a>
+        <div style={{ cursor: "pointer" }} onClick={() => openLinkInNewTab(supplier.siteLink)}>
+          <CardMedia 
+            sx={{ height: 150, width: 300}} 
+            image={supplier.imageUrl} 
+            title={supplier.name} 
+            component="img"
+            onDragStart={(e) => e.stopPropagation()}
+          />
+        </div>
         <CardContent>
           <Typography gutterBottom variant="h5" component="div" align="center">
             {supplier.name}
@@ -22,3 +33,4 @@ export const Supplier = ({ supplier }) => {
     </Grid>
   );
 };
+
